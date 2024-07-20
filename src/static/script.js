5e6cb365b5f4f600e8c581f7e085a8c2a7982934
bonbon <mail@jan-schmitt.eu>
2024-07-20 16:56:55 +0200

    added waiting animation

--- src/static/script.js
+++ src/static/script.js
@@ -1,11 +1,12 @@
 $("#myForm").submit(function (e) {
+    document.getElementById('image').style.display = 'block';
     e.preventDefault();
     $.ajax({
         url: '/submit',
         type: 'post',
         data: $('#myForm').serialize(),
         success: function () {
-            alert("worked");
+            document.getElementById('image').style.display = 'none';
         }
     });
 });
