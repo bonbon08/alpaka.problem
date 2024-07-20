331e118d9edd45acafe3cbd546008544de865929
bonbon <mail@jan-schmitt.eu>
2024-07-20 18:33:58 +0200

    edited word count edited promt

--- src/gpt.py
+++ src/gpt.py
@@ -19,10 +19,10 @@ def get_ai_return(trolllevel, theme):
     ]
     promt = "Mein Thema ist: " + theme + " und es soll ernsthafftigkeit " + seriousnesses[trolllevel] +" haben, bitte halte die formulierung der worte und wort wahl auf dem nivou eines durchnittlichen 12-18 jährigen"
     sys_promt="""
-    You are a chat model that should gennerate problems for the user to solve with code, you should not output any code but only a problem,
+    You are a chat model that should gennerate a single problem for the user to solve with code, you should not output any code but only a problem,
     you should output what the solution could be, what problem it solves, what technolgies the solution might use and what the the user might
     need to make this solution please make for each of each of these a Title and after that print the information the Titles should be (german):
-    Endprodunkt, Problem/Fragestellung, Daten&technologien and was fehlt uns create a maximum of 1 sentence for each title keep your answers as short maximum for everythign is 120 words?
+    Endprodunkt, Problem/Fragestellung, Daten&technologien and was fehlt uns create a maximum of 1 sentence for each title keep your answers as short maximum for everythign is 80 words?
     . Your output should be directly printable on a poster.
     Only create one single problem and not more because we only want on problem so just create one and not two make only one or two senteces for each title
     your answer should be in german and you should make it a little bit trolly if the user wants to 
